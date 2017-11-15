cbb5ef4a83773ed2f68b9fc5c7a680a2d91ee0ca
datagutten <datagutten@datagutten.net>
2017-11-15 12:23:57 +0100

    Added sample file

---
+++ school_groups_sample.php
@@ -0,0 +1,4 @@
+<?php
+//Hvilken gruppe i AD skal ha tilgang til hvilken skole
+$school_groups['unit_534907f5-95e3-4df6-97e4-14bf1aded9a5']='CN=SkoleGruppe,OU=Larer,OU=Skole,OU=Grupper,DC=domene,DC=no';
+$school_groups['all']='CN=Felles-IT,OU=Grupper,DC=domene,DC=no';
